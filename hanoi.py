b8512abe83852d7c1217b5ccdcebd0c5f9b41505
Fernando_data_CL <73721626+FernandoCL1991@users.noreply.github.com>
2023-02-26 12:19:20 +0100

    Uploading final commit

--- hanoi.py
+++ hanoi.py
@@ -2,13 +2,24 @@
 """
 Created on Mon Feb 20 08:24:20 2023
 
-@author: Hannah
+@author: Fernando Corral Lozada and Danial Riaz
 """
 
-def TowerOfHanoi(n , from_rod, to_rod, aux_rod): 
-    
-    #delete the pass and replace with your recursive algorithm
-    pass
+if __name__ == "__main__":
 
+    # Move/print function
+    def move(from_rod, to_rod):
+        print(f"Move disc from {from_rod} to {to_rod}")
 
-TowerOfHanoi(3, 'A', 'C', 'B')  
+    # Recursion function
+    def TowerOfHanoi(n, from_rod, aux_rod, to_rod):
+        if n == 0:
+            pass
+        else:
+            TowerOfHanoi(n-1, from_rod, to_rod, aux_rod)
+            move(from_rod, to_rod)
+            TowerOfHanoi(n-1, aux_rod, from_rod, to_rod)
+
+print("Welcome to Tower of Hanoi!")
+n = int(input("Enter how many disks you want to play with: "))
+TowerOfHanoi(n, "A", "B", "C")
